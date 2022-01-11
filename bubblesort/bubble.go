64dbbe1b0858025490aca6f505df96f90c11ada6
Jason Salas <jason@kuam.com>
2022-01-11 17:35:55 +1000

    optimizations-1
    don't compare numbers that are already in their final sorted position

--- bubblesort/bubble.go
+++ bubblesort/bubble.go
@@ -4,16 +4,16 @@ package bubblesort
 func BubbleSort(numbers []int) {
 	var i int
 	for i = 0; i < len(numbers); i++ {
-		Sweep(numbers)
+		Sweep(numbers, i)
 	}
 }
 
-func Sweep(numbers []int) {
+func Sweep(numbers []int, previousPass int) {
 	var n int = len(numbers)
 	var firstIndex int = 0
 	var secondIndex int = 1
 
-	for secondIndex < n {
+	for secondIndex < (n - previousPass) {
 		var firstNumber int = numbers[firstIndex]
 		var secondNumber int = numbers[secondIndex]
 
