176cd4efb90f01beb6f25f7ce4a33e99d991d827
Jason Salas <jason@kuam.com>
2022-01-14 08:26:41 +1000

    test cases

--- binarysearch/binarysearch_test.go
+++ binarysearch/binarysearch_test.go
@@ -5,11 +5,25 @@ import (
 )
 
 func TestBinarySearch(t *testing.T) {
-	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	got := BinarySearch(nums, 4)
-	want := 3
+	testCases := []struct {
+		sortedList []int
+		lookFor    int
+		index      int
+	}{
+		{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}, 3, 2},
+		{[]int{1, 2, 3, 4, 5}, 5, 4},
+		{[]int{0, 6, 7, 8}, 3, -1},
+		{[]int{0, 1}, 1, 1},
+		{[]int{1}, 3, -1},
+		{[]int{}, 3, -1},
+	}
 
-	if got != want {
-		t.Errorf("got: %d | want: %d", got, want)
+	for _, tt := range testCases {
+		got := BinarySearch(tt.sortedList, tt.lookFor)
+		want := tt.index
+
+		if got != want {
+			t.Errorf("got: %d | want: %d", got, want)
+		}
 	}
 }
